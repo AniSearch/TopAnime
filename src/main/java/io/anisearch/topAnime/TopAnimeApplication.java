b2aa5a729a80224a1abd4174dcdfacba2801afd1
Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>
2022-12-10 03:15:37 +0530

    Initial Commit
    Signed-off-by: Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>

---
+++ src/main/java/io/anisearch/topAnime/TopAnimeApplication.java
@@ -0,0 +1,13 @@
+package io.anisearch.topAnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopAnimeApplication.class, args);
+	}
+
+}
