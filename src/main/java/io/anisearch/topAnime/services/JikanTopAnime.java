5a5c0deca06a7d4d29fe348e2a5bc6a23fed264e
Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>
2022-12-10 19:33:21 +0530

    Port added to the Springboot Application
    Signed-off-by: Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>

---
+++ src/main/java/io/anisearch/topAnime/services/JikanTopAnime.java
@@ -0,0 +1,8 @@
+package io.anisearch.topAnime.services;
+
+import org.springframework.web.client.RestTemplate;
+
+public class JikanTopAnime {
+    RestTemplate restTemplate =new RestTemplate();
+    
+}
